05b9730a604e14b51f36ea72d4c6c8012bc83863
Fil <concatofilippo94@gmail.com>
2023-08-18 15:35:19 +0200

    navbar almost completed. components need links to page

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -6,7 +6,9 @@ import {ReactComponent as ProjectImg } from "./assets/diagram-project-solid.svg"
 import {ReactComponent as MoreImg } from "./assets/icons-solid.svg";
 import {ReactComponent as ContactImg } from "./assets/address-book-solid.svg";
 import meImageNoBg from "./assets/me-nobg.png"
-
+import {ReactComponent as GitHub} from "./assets/github.svg";
+import {ReactComponent as Instagram} from "./assets/instagram.svg";
+import {ReactComponent as Linkedin} from "./assets/linkedin.svg";
 
 
 const Navbar = () => {
@@ -16,6 +18,12 @@ const Navbar = () => {
         { text: "More", icon: <MoreImg className="desktopListImg" /> },
         { text: "Contact", icon: <ContactImg className="desktopListImg" /> },
     ]);
+
+    const[socials] = useState([
+        {text: "GitHub", icon: <GitHub className="socialImg" /> },
+        {text: "Instagram", icon: <Instagram className="socialImg" /> },
+        {text: "Linkedin", icon: <Linkedin className="socialImg" /> },
+    ])
 
     return (
         <div className="containerNavbar">
@@ -28,12 +36,22 @@ const Navbar = () => {
                         <div key={index} className="desktopMenuListItem bordersRules">
                             <Link>
                                 <div className="menuLinkWrapper">
-                                    <span className="desktopMenuenuListItemIcon">{menuItem.icon}</span>
-                                    <span className="desktopMenuenuListItemText">{menuItem.text}</span>
+                                    <span className="desktopMenuListItemIcon">{menuItem.icon}</span>
+                                    <span className="desktopMenuListItemText">{menuItem.text}</span>
                                 </div>
                             </Link>
                         </div>
+
                     ))}
+                </div>
+                <div className="social_media">
+                        {socials.map((social, index) =>(
+                            <Link>
+                                <div key={index}>
+                                    {social.icon}
+                                </div>
+                    </Link>
+                        ))}
                 </div>
             </nav>
         </div>
