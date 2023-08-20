21ca9b49f5a03f306f5f13f9f26678daf49d6a3b
Fil <concatofilippo94@gmail.com>
2023-08-20 19:10:11 +0200

    adding external link to socials

--- src/UI/socials/Socials.js
+++ src/UI/socials/Socials.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Link } from "react-scroll";
+import {ExternalLink} from "react-external-link";
 import { socialsData } from "./socialsData";
 
 const Socials = () => {
     return (
         <div className="social_media">
             {socialsData.map((social, index) => (
-                <Link>
+
                     <div key={index}>
                         {social.icon}
                     </div>
-                </Link>
+
             ))}
         </div>
     )
--- src/UI/socials/socialsData.js
+++ src/UI/socials/socialsData.js
@@ -2,8 +2,9 @@ import {ReactComponent as GitHub} from "./assets/github.svg";
 import {ReactComponent as Instagram} from "./assets/instagram.svg";
 import {ReactComponent as Linkedin} from "./assets/linkedin.svg";
 
+
 export const socialsData = [
-    {text: "GitHub", icon: <GitHub className="socialImg" /> },
-    {text: "Instagram", icon: <Instagram className="socialImg" /> },
-    {text: "Linkedin", icon: <Linkedin className="socialImg" /> },
+    {text: "GitHub", icon: <GitHub className="socialImg" />, link: "https://github.com/fppcnc"},
+    {text: "Instagram", icon: <Instagram className="socialImg" />, link: "https://www.instagram.com/fppcnc/" },
+    {text: "Linkedin", icon: <Linkedin className="socialImg" />, link: "https://www.linkedin.com/in/filippo-concato-a675b4282/" },
 ]
