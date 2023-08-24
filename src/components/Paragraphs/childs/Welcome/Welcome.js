a38f36842ad12685d3f197f6f43a5a406245e3db
Fil <concatofilippo94@gmail.com>
2023-08-24 13:17:10 +0200

    added welcome section with Typewriter component

--- src/components/Paragraphs/childs/Welcome/Welcome.js
+++ src/components/Paragraphs/childs/Welcome/Welcome.js
@@ -1,6 +1,7 @@
 import {Typewriter} from "react-simple-typewriter";
 import GraphemeSplitter from "grapheme-splitter";
 import "./welcome.css"
+import {sectionsData} from "../../../../data/sectionsData";
 
 const Welcome = () => {
     const stringSplitter = (string) => {
@@ -17,9 +18,12 @@ const Welcome = () => {
                 words={[
                     "I'm Filippo and welcome on my Web-Page",
                     "I'm an enthusiast student and Web content creator",
-                    "What you see here has been created by me",
+                    "What you see here has been entirely created by me",
                     "Scroll down and have a look at my portfolio",
-                    "Enjoy!"]
+
+                    "Enjoy!",
+                    "You can easily reach me via email or via social networks. You will find everything in the Contact Section"]
+
                 }
                 typeSpeed={60}
                 deleteSpeed={50}
