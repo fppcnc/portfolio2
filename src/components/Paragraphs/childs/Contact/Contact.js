a4d34e16f606eb8474172463df98441b3a36d254
Fil <concatofilippo94@gmail.com>
2023-08-31 00:13:00 +0200

    Home content moved to About

--- src/components/Paragraphs/childs/Contact/Contact.js
+++ src/components/Paragraphs/childs/Contact/Contact.js
@@ -3,6 +3,16 @@ import React from "react";
 const Contact = () => {
     return (
         <>
+            email: email
+
+            socials:
+             -gitbub
+             -insta
+             -linkedin
+
+            support:
+            paypal.me: (https://paypal.me/fppcnc?country.x=DE&locale.x=en_US)
+            github sponsor:
         </>
     )
 }
