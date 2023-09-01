a4022e2c5e2ecd90bb308a19acef9778aa4100a0
Fil <concatofilippo94@gmail.com>
2023-09-01 18:56:47 +0200

    adding contact links

--- src/components/Paragraphs/childs/Contact/Contact.js
+++ src/components/Paragraphs/childs/Contact/Contact.js
@@ -5,8 +5,9 @@ import {ReactComponent as GitHubLogo} from './contatLogos/github_logo.svg';
 import {ReactComponent as InstagramLogo} from './contatLogos/instagram_logo.svg';
 import {ReactComponent as LinkedInLogo} from './contatLogos/linkedin_logo.svg';
 import {ReactComponent as RedditLogo} from './contatLogos/reddit_logo.svg';
-import {ReactComponent as PayPalMeLogo} from './contatLogos/paypalme_logo.svg';
+import {ReactComponent as PayPalLogo} from './contatLogos/paypal_logo.svg';
 import {ReactComponent as GitHubSponsorLogo} from './contatLogos/githubsponsor_logo.svg';
+import {ReactComponent as CopyLogo} from "./contatLogos/copy.svg";
 
 const Contact = () => {
 
@@ -20,20 +21,19 @@ const Contact = () => {
             <label>Email</label>
             <section className="email-section">
                 <input type="text" value="concatofilippo94@gmail.com" readOnly />
-                <button onClick={copyEmailToClipboard}><img src="./contactLogos/copy.svg" alt="Copy Icon" /></button>
+                <button onClick={copyEmailToClipboard}><CopyLogo /></button>
             </section>
-
+            <label>Socials</label>
             <section className="socials-section">
-                <label>Socials</label>
-                <a href="https://github.com/yourUsername" target="_blank" rel="noopener noreferrer"><GitHubLogo /><p>@fppcnc</p></a>
-                <a href="https://instagram.com/yourUsername" target="_blank" rel="noopener noreferrer"><InstagramLogo /><p>@fppcnc</p></a>
-                <a href="https://linkedin.com/in/yourUsername" target="_blank" rel="noopener noreferrer"><LinkedInLogo /><p>@fppcnc</p></a>
-                <a href="https://reddit.com/u/yourUsername" target="_blank" rel="noopener noreferrer"><RedditLogo /><p>@fppcnc</p></a>
+                <a href="https://github.com/fppcnc" target="_blank" rel="noopener noreferrer"><GitHubLogo /><p>#fppcnc</p></a>
+                <a href="https://www.instagram.com/fppcnc/" target="_blank" rel="noopener noreferrer"><InstagramLogo /><p>@fppcnc</p></a>
+                <a href="https://www.linkedin.com/in/filippo-concato-a675b4282/" target="_blank" rel="noopener noreferrer"><LinkedInLogo /><p>filippo-concato</p></a>
+                <a href="https://www.reddit.com/user/Fel_ope" target="_blank" rel="noopener noreferrer"><RedditLogo /><p>u/Fel_ope</p></a>
             </section>
             <label>Support</label>
             <section className="support-section">
-                <a href="https://paypal.me/yourUsername" target="_blank" rel="noopener noreferrer"><PayPalMeLogo /><p>paypal.me</p></a>
-                <a href="https://github.com/sponsors/yourUsername" target="_blank" rel="noopener noreferrer"><GitHubSponsorLogo /><p>GitHub Sponsor</p></a>
+                <a href="https://paypal.me/fppcnc?country.x=DE&locale.x=en_US" target="_blank" rel="noopener noreferrer"><PayPalLogo /><p>paypal.me</p></a>
+                <a href="https://github.com/fppcnc" target="_blank" rel="noopener noreferrer"><GitHubSponsorLogo /><p>GitHub Sponsor</p></a>
             </section>
         </div>
     );
