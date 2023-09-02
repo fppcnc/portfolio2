c8951f5e56cab5b7324ce369ef71816491769d3c
Fil <concatofilippo94@gmail.com>
2023-09-02 19:25:48 +0200

    ready for deploy

--- src/components/Paragraphs/childs/Home/Home.js
+++ src/components/Paragraphs/childs/Home/Home.js
@@ -7,8 +7,12 @@ const Home = () => {
 
     return (
 
-        <>
-
+        <>@to do
+            <ul>
+<li>What i do</li>
+    <li>    language proficiency chart</li>
+    <li> programming language proficiency</li>
+            </ul>
         </>
     )
 }
