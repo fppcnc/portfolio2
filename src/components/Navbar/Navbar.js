71d36e9f5e47f8e17ed81d5261370fe385d6e39f
Fil <concatofilippo94@gmail.com>
2023-09-04 18:35:56 +0200

    ready for deploy

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import MenuItems from "./childs/MenuItems";
 import Socials from "./childs/Socials";
 import Me from "./childs/Me";
 
 const Navbar = (props) => {
-    const [isNavbarVisible, setNavbarVisibility] = useState(true);
-
-    const toggleNavbar = () => {
-        setNavbarVisibility(!isNavbarVisible);
-    };
 
     return (
-        <div>
-            <button className="navbar-toggle-button" onClick={toggleNavbar}>
-                ☰
-            </button>
-            <div className={`containerNavbar ${!isNavbarVisible ? 'hidden' : ''}`}>
-                <nav className="navbar">
-                    <Me />
-                    <MenuItems sectionsData={props.sectionsData} singleComponent={{ title: "Welcome", icon: " " }} />
-                    <Socials />
-                </nav>
-            </div>
+        <div className="containerNavbar">
+            <nav className="navbar">
+                <Me/>
+                <MenuItems sectionsData={props.sectionsData} singleComponent={{ title: "Welcome", icon: " " }} />
+                <Socials />
+            </nav>
         </div>
-    );
+    )
 }
 
 export default Navbar;
